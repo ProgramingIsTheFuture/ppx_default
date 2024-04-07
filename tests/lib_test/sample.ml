f7426cbc188b14917fd8017cb90837c3110de579
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2024-04-07 13:11:09 +0100

    ppx_default: adding support for list, array, string, char, tupple, fun, float

--- tests/lib_test/sample.ml
+++ tests/lib_test/sample.ml
@@ -1,3 +1,13 @@
-type abc = { test_me : int; name : string } [@@deriving show, default]
+type abc = {
+  test_me : int;
+  name : string;
+  tup : int * string;
+  calculate : string -> int -> float -> int;
+  arr : string array;
+  l : int list;
+}
+[@@deriving show, default]
 
-let _ = abc_default () |> show_abc |> print_string |> print_newline |> flush_all
+let _ =
+  let abc = abc_default () in
+  abc |> show_abc |> print_string |> print_newline |> flush_all
